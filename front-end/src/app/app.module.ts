09e253ff77a9b0dbb2f520f5f335eb0f8238915a
asantos <asantos@dataweb.com.br>
2021-12-31 13:10:55 -0300

    Web: Adicionado font-awesome e importação de icones.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { DataPipe } from './date.pipe';
+import { AppService } from './app.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,24 +12,31 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { CreateTaskModalComponent } from './components/create-task-modal/create-task-modal.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import icons from './icons';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    CreateTaskModalComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    NgbModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HeaderComponent, HomeComponent, CreateTaskModalComponent, DataPipe],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        NgbModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        FontAwesomeModule,
+        ToastrModule.forRoot()
+    ],
+    providers: [AppService, DataPipe],
+    bootstrap: [AppComponent],
+    exports: [DataPipe]
 })
-export class AppModule {}
+export class AppModule {
+    constructor(iconLibrary: FaIconLibrary) {
+        icons.forEach((i) => iconLibrary.addIcons(i));
+    }
+}
---
+++ front-end/src/app/icons.ts
@@ -0,0 +1,5 @@
+import { faThumbsUp, faEdit, faTrashAlt, faFileAlt } from '@fortawesome/pro-regular-svg-icons';
+
+const icons = [faThumbsUp, faEdit, faTrashAlt, faFileAlt];
+
+export default icons;
