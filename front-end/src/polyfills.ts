cfe9ca014ba5a09825d2e36752994771cb10ad82
Asantoss <adrieldossantos7@gmail.com>
2022-01-03 21:47:56 -0300

    Web: Adiciona importação localizem.

--- front-end/src/polyfills.ts
+++ front-end/src/polyfills.ts
@@ -13,7 +13,7 @@
  *
  * Learn more in https://angular.io/guide/browser-support
  */
-
+import '@angular/localize/init';
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
@@ -55,8 +55,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
