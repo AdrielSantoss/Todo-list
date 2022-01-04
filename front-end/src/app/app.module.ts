fd15eefac2dc2fafde9fa8b1372aa91bd30c50dc
Asantoss <adrieldossantos7@gmail.com>
2022-01-03 21:48:37 -0300

    Web: Adiciona format de data em provider de AppModule.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { DataPipe } from './date.pipe';
+import { DataPipe } from './utils/date.pipe';
 import { AppService } from './app.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -10,13 +10,14 @@ import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { TaskModalComponent } from './components/task-modal/task-modal.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateParserFormatter, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
-import icons from './icons';
+import icons from './utils/icons';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoaderComponent } from './components/loader/loader.component';
+import { DateFormat } from './utils/date.format';
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent, HomeComponent, TaskModalComponent, DataPipe, LoaderComponent],
@@ -35,7 +36,7 @@ import { LoaderComponent } from './components/loader/loader.component';
             positionClass: 'toast-bottom-right'
         })
     ],
-    providers: [AppService, DataPipe, ToastrService],
+    providers: [AppService, DataPipe, ToastrService, { provide: NgbDateParserFormatter, useClass: DateFormat }], //ngbDatePicker date format pt-br (dd-mm-yyy)
     bootstrap: [AppComponent],
     exports: [DataPipe]
 })
